2e5cefee88bd03ada03944c7860fd34a26e0b80e
gorkemyagci <gorkemyagci705@gmail.com>
2025-03-05 16:32:01 +0300

    upwork-frontend

--- modules/auth/ui/components/submit-button.tsx
+++ modules/auth/ui/components/submit-button.tsx
@@ -12,7 +12,7 @@ const SubmitButton = ({ text }: SubmitButtonProps) => {
             bg-gradient-to-b from-[#20232D]/90 to-[#20232D]
             border border-[#515256] shadow-[0_1px_2px_0_rgba(27,28,29,0.05)]"
         >
-            <div className="absolute top-0 left-0 w-full h-0 group-hover:h-4 transition-all duration-500 bg-gradient-to-b from-[#FFF]/[0.09] to-[#FFF]/0" />
+            <div className="absolute top-0 left-0 w-full h-3 group-hover:h-5 transition-all duration-500 bg-gradient-to-b from-[#FFF]/[0.09] group-hover:from-[#FFF]/[0.12] to-[#FFF]/0" />
             {text}
         </Button>
     )
