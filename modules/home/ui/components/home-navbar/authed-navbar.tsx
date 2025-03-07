852254a122790a746295f1c6a784bcb4240d62a2
gorkemyagci <gorkemyagci705@gmail.com>
2025-03-07 16:21:14 +0300

    upwork

--- modules/home/ui/components/home-navbar/authed-navbar.tsx
+++ modules/home/ui/components/home-navbar/authed-navbar.tsx
@@ -2,20 +2,22 @@ import { motion } from "framer-motion";
 import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
 import ProfileDropdown from "./profile-dropdown";
+import Link from "next/link";
 
 const AuthedNavbar = () => {
     return (
         <>
-            <Button
-                type="submit"
-                className="h-10 group flex items-center gap-2 rounded-[10px] text-white text-sm cursor-pointer font-medium relative overflow-hidden transition-all
+            <Link href="/job-posting" prefetch>
+                <Button
+                    type="submit"
+                    className="h-10 group flex items-center gap-2 rounded-[10px] text-white text-sm cursor-pointer font-medium relative overflow-hidden transition-all
                 bg-gradient-to-b from-[#20232D]/90 to-[#20232D]
                 border border-[#515256] shadow-[0_1px_2px_0_rgba(27,28,29,0.05)]"
-            >
-                <div className="absolute top-0 left-0 w-full h-0 group-hover:h-4 transition-all duration-500 bg-gradient-to-b from-[#FFF]/[0.09] to-[#FFF]/0" />
-                <Icons.plus className="fill-white size-3" />
-                <span className="text-sm font-medium">Create Request</span>
-            </Button>
+                >
+                    <div className="absolute top-0 left-0 w-full h-0 group-hover:h-4 transition-all duration-500 bg-gradient-to-b from-[#FFF]/[0.09] to-[#FFF]/0" />
+                    <Icons.plus className="fill-white size-3" />
+                    <span className="text-sm font-medium">Create Request</span>
+                </Button></Link>
             <motion.span
                 className="cursor-pointer hidden sm:block"
             >
