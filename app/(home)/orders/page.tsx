0c5a1205e57da89552171ade118ae4064141e9bc
gorkemyagci <gorkemyagci705@gmail.com>
2025-03-10 15:05:17 +0300

    innhee-frontend

--- app/(home)/orders/page.tsx
+++
@@ -1,9 +0,0 @@
-import Billing from "@/modules/settings-modal/ui/sections/billing";
-
-const Page = () => {
-    return <div className="w-full max-w-[1440px] mx-auto pb-10">
-        <Billing />
-    </div>
-}
-
-export default Page;
